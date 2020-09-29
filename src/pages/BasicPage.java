bfd59e7a25c537851e71aad46617d544b1a69d3d
Mladen Cekic <cekicj@live.com>
2020-09-29 19:01:00 +0200

    WIP Pages

--- src/pages/BasicPage.java
+++ src/pages/BasicPage.java
@@ -1,5 +1,18 @@
 package pages;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public abstract class BasicPage {
 
+	protected WebDriver driver;
+	protected WebDriverWait wait;
+	protected JavascriptExecutor executor;
+	
+	public BasicPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor executor) {
+		this.driver = driver;
+		this.wait = wait;
+		this.executor = executor;
+	}
 }
---
+++ src/pages/LoginPage.java
@@ -0,0 +1,35 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginPage extends BasicPage{
+
+	public LoginPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor executor) {
+		super(driver, wait, executor);
+	}
+
+	public WebElement getEmailInput() {
+		return this.driver.findElement(By.name("username"));
+	}
+	
+	public WebElement getPasswordInput() {
+		return this.driver.findElement(By.name("password"));
+	}
+	
+	public WebElement getLoginButton() {
+		return this.driver.findElement(By.name("btn_submit"));
+	}
+	
+	public void login(String email, String password) {
+		this.getEmailInput().clear();
+		this.getEmailInput().sendKeys(email);
+		this.getPasswordInput().clear();
+		this.getPasswordInput().sendKeys(password);
+		this.getLoginButton().click();
+	}
+	
+}
