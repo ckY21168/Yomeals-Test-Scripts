0c26f68ca3000f949095dd66b6e557181498ba67
Mladen Cekic <cekicj@live.com>
2020-10-01 17:28:45 +0200

    Add meal to favourite & clear cart TESTS added

--- src/tests/MealTest.java
+++ src/tests/MealTest.java
@@ -1,27 +1,28 @@
 package tests;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Assert;
 import org.testng.annotations.Test;
 
-import pages.AuthenticationPage;
+import pages.CartSummaryPage;
 import pages.LocationPopUpPage;
 import pages.LoginPage;
 import pages.MealPage;
 import pages.NotificationSystemPage;
-import pages.ProfilePage;
 
 public class MealTest extends BasicTest {
 
 	@Test(priority = 0)
 	public void addMealToCart() throws IOException, InterruptedException {
 		LocationPopUpPage locationPopUpPage = new LocationPopUpPage(this.driver, this.wait, this.executor);
-		LoginPage loginPage = new LoginPage(this.driver, this.wait, this.executor);
 		NotificationSystemPage notificationSystemPage = new NotificationSystemPage(this.driver, this.wait,
 				this.executor);
-		ProfilePage profilePage = new ProfilePage(this.driver, this.wait, this.executor);
-		AuthenticationPage authenticationPage = new AuthenticationPage(this.driver, this.wait, this.executor);
 		MealPage mealPage = new MealPage(this.driver, this.wait, this.executor);
 
 		this.driver.navigate().to(baseUrl + "meal/lobster-shrimp-chicken-quesadilla-combo");
@@ -42,4 +43,90 @@ public class MealTest extends BasicTest {
 		Assert.assertTrue(notificationSystemPage.notificationMessage().contains("Meal Added To Cart"));
 
 	}
+
+	@Test(priority = 5)
+	public void addMealToFavourite() throws IOException, InterruptedException {
+		LocationPopUpPage locationPopUpPage = new LocationPopUpPage(this.driver, this.wait, this.executor);
+		LoginPage loginPage = new LoginPage(this.driver, this.wait, this.executor);
+		NotificationSystemPage notificationSystemPage = new NotificationSystemPage(this.driver, this.wait,
+				this.executor);
+		MealPage mealPage = new MealPage(this.driver, this.wait, this.executor);
+
+		this.driver.navigate().to(baseUrl + "meal/lobster-shrimp-chicken-quesadilla-combo");
+
+		locationPopUpPage.closeLocationHeader();
+
+		mealPage.favouriteMeal();
+
+		Assert.assertTrue(notificationSystemPage.notificationMessage().contains("Please login first!"));
+
+		notificationSystemPage.systemMessageDisappear();
+
+		this.driver.navigate().to(baseUrl + "guest-user/login-form");
+
+		loginPage.login(email, password);
+
+		this.driver.navigate().to(baseUrl + "meal/lobster-shrimp-chicken-quesadilla-combo");
+
+		mealPage.favouriteMeal();
+
+		Assert.assertTrue(
+				notificationSystemPage.notificationMessage().contains("Product has been added to your favorites"));
+	}
+
+	@Test(priority = 10)
+	public void clearCart() throws IOException, InterruptedException {
+		CartSummaryPage cartSummaryPage = new CartSummaryPage(this.driver, this.wait, this.executor);
+		LocationPopUpPage locationPopUpPage = new LocationPopUpPage(this.driver, this.wait, this.executor);
+		NotificationSystemPage notificationSystemPage = new NotificationSystemPage(this.driver, this.wait,
+				this.executor);
+		MealPage mealPage = new MealPage(this.driver, this.wait, this.executor);
+
+		this.driver.navigate().to(baseUrl + "meals");
+
+		locationPopUpPage.closeLocationHeader();
+
+		locationPopUpPage.setLocation("City Center - Albany");
+
+		File file = new File("data/Data.xlsx").getCanonicalFile();
+		FileInputStream fis = new FileInputStream(file);
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet sheet1 = wb.getSheet("Meals");
+
+		for (int i = 1; i <= sheet1.getLastRowNum(); i++) {
+			String newTabScript = "window.open();";
+			executor.executeScript(newTabScript);
+
+			String mainWindowHandles = driver.getWindowHandle();
+			String newMealWindowHandles = "";
+
+			String url = sheet1.getRow(i).getCell(0).getStringCellValue();
+			double quantityDouble = sheet1.getRow(i).getCell(1).getNumericCellValue();
+
+			int quantityInt = (int) quantityDouble;
+
+			ArrayList<String> windowHandlesList = new ArrayList<String>(driver.getWindowHandles());
+			windowHandlesList.remove(mainWindowHandles);
+			newMealWindowHandles = windowHandlesList.get(0);
+
+			driver.switchTo().window(newMealWindowHandles);
+			driver.navigate().to(url);
+
+			mealPage.addMeal(quantityInt);
+
+			Thread.sleep(3000);
+
+			softAssert.assertTrue(notificationSystemPage.notificationMessage().contains("Meal Added To Cart"),
+					"[ERROR] Meal Not Added");
+
+			notificationSystemPage.systemMessageDisappear();
+		}
+		softAssert.assertAll();
+		wb.close();
+		fis.close();
+		cartSummaryPage.clearAll();
+		Assert.assertTrue(
+				notificationSystemPage.notificationMessage().contains("All meals removed from Cart successfully"));
+	}
+
 }
