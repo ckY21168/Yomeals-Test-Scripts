b0a71d3927ea48528019fc8fc754593796d40445
Mladen Cekic <cekicj@live.com>
2020-10-01 16:11:43 +0200

    Profile Image test added

--- src/tests/ProfileTest.java
+++ src/tests/ProfileTest.java
@@ -3,6 +3,8 @@ package tests;
 import java.io.IOException;
 
 import org.junit.Assert;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
 import pages.AuthenticationPage;
@@ -11,34 +13,73 @@ import pages.LoginPage;
 import pages.NotificationSystemPage;
 import pages.ProfilePage;
 
-public class ProfileTest extends BasicTest{
-	
+public class ProfileTest extends BasicTest {
+
 	@Test(priority = 0)
 	public void editProfile() throws IOException, InterruptedException {
 		LocationPopUpPage locationPopUpPage = new LocationPopUpPage(this.driver, this.wait, this.executor);
 		LoginPage loginPage = new LoginPage(this.driver, this.wait, this.executor);
-		NotificationSystemPage notificationSystemPage = new NotificationSystemPage(this.driver, this.wait, this.executor);
+		NotificationSystemPage notificationSystemPage = new NotificationSystemPage(this.driver, this.wait,
+				this.executor);
 		ProfilePage profilePage = new ProfilePage(this.driver, this.wait, this.executor);
 		AuthenticationPage authenticationPage = new AuthenticationPage(this.driver, this.wait, this.executor);
-		
+
 		this.driver.navigate().to(baseUrl + "guest-user/login-form");
-			
+
 		locationPopUpPage.closeLocationHeader();
-		
+
 		loginPage.login(email, password);
-		
+
 		Assert.assertTrue(notificationSystemPage.notificationMessage().equalsIgnoreCase("Login Successfull"));
-		
+
 		this.driver.navigate().to(baseUrl + "member/profile/");
-					
-		profilePage.profileUpdate("Mladen", "Cekic", "Naissus", "0658430036" , 18000, "United States", "California", "Los Angeles");
-		
+
+		profilePage.profileUpdate("Mladen", "Cekic", "Naissus", "0658430036", 18000, "United States", "California",
+				"Los Angeles");
+
 		Assert.assertTrue(notificationSystemPage.notificationMessage().equalsIgnoreCase("Setup Successfull"));
-		
+
 		authenticationPage.logout();
-		
+
 		Assert.assertTrue(notificationSystemPage.notificationMessage().equalsIgnoreCase("Logout Successfull"));
 	}
-	
+
+	@Test(priority = 5)
+	public void profileImageChange() throws IOException, InterruptedException {
+		LocationPopUpPage locationPopUpPage = new LocationPopUpPage(this.driver, this.wait, this.executor);
+		LoginPage loginPage = new LoginPage(this.driver, this.wait, this.executor);
+		NotificationSystemPage notificationSystemPage = new NotificationSystemPage(this.driver, this.wait,
+				this.executor);
+		ProfilePage profilePage = new ProfilePage(this.driver, this.wait, this.executor);
+		AuthenticationPage authenticationPage = new AuthenticationPage(this.driver, this.wait, this.executor);
+
+		this.driver.navigate().to(baseUrl + "guest-user/login-form");
+
+		locationPopUpPage.closeLocationHeader();
+
+		loginPage.login(email, password);
+
+		Assert.assertTrue(notificationSystemPage.notificationMessage().contains("Login Successfull"));
+
+		this.driver.navigate().to(baseUrl + "member/profile/");
+
+		profilePage.photoUpload();
+
+		Assert.assertTrue(
+				notificationSystemPage.notificationMessage().contains("Profile Image Uploaded Successfully"));
+
+		notificationSystemPage.systemMessageDisappear();
+
+		profilePage.imageRemove();
+
+		Assert.assertTrue(
+				notificationSystemPage.notificationMessage().contains("Profile Image Deleted Successfully"));
+
+		notificationSystemPage.systemMessageDisappear();
+
+		authenticationPage.logout();
+
+		Assert.assertTrue(notificationSystemPage.notificationMessage().contains("Logout Successfull"));
+	}
 
 }
